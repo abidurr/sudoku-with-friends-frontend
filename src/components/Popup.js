d80ed9043c8442b146100cf3443f5eab4d7c0b46
abidurr <42abid@gmail.com>
2020-05-17 00:45:35 -0400

    Nicer popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,16 +3,19 @@ import React from "react";
 export default function Popup({show, onPopupClose, res}) {
     if (show === true) {
         let msg = "";
+        let time = res.time;
+        console.log(msg, time);
         if (res.verdict === true) {
-            msg = `Congrats! You finished the puzzle.`;
+            msg = "Congrats! You finished the puzzle.";
         } else {
             msg = "Your final guess is wrong!";
         }
         return (
             <div class="popup">
-                <p>{msg} <button onClick={() => {onPopupClose()}}>
+                <p>{msg} </p> 
+                <button onClick={() => {onPopupClose()}}>
                     Okay
-                </button> </p>
+                </button> 
                 
             </div>
         )
