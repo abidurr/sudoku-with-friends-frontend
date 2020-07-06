417edcb28040a8184d55271eccc7aa99350b0614
Raihanul Islam Refat <refat0167@gmail.com>
2020-07-06 23:02:07 +0600

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
-import * as store from "./store";
-import Cell from "./components/Cell";
-import Popup from "./components/Popup";
+import React from 'react';
+import * as store from './store';
+import Cell from './components/Cell';
+import Popup from './components/Popup';
+
+const secondsToHms = d => {
+    d = Number(d);
+    const h = Math.floor(d / 3600);
+    const m = Math.floor((d % 3600) / 60);
+    const s = Math.floor((d % 3600) % 60);
+    const hDisplay = h > 0 ? h + (h === 1 ? ' hr ' : ' hrs ') : '';
+    const mDisplay = m > 0 ? m + (m === 1 ? ' min ' : ' mins ') : '';
+    const sDisplay = s > 0 ? s + (s === 1 ? ' sec' : ' secs') : '';
+    return hDisplay + mDisplay + sDisplay;
+};
 
 class App extends React.Component {
     constructor(props) {
@@ -15,7 +26,6 @@ class App extends React.Component {
             finishTime: -1,
             showPopup: false,
         };
-        this.countUp = this.countUp.bind(this);
         setInterval(this.countUp, 1000);
     }
 
@@ -29,54 +39,51 @@ class App extends React.Component {
         store.updateCell(row, col, newVal);
     };
 
-    secondsToHms(d) {
-        d = Number(d);
-        const h = Math.floor(d / 3600);
-        const m = Math.floor((d % 3600) / 60);
-        const s = Math.floor((d % 3600) % 60);
-        const hDisplay = h > 0 ? h + (h === 1 ? " hr " : " hrs ") : "";
-        const mDisplay = m > 0 ? m + (m === 1 ? " min " : " mins ") : "";
-        const sDisplay = s > 0 ? s + (s === 1 ? " sec" : " secs") : "";
-        return hDisplay + mDisplay + sDisplay;
-    }
-
-    countUp() {
+    countUp = () => {
         this.setState(({ time }) => ({ time: time + 1 }));
-    }
+    };
+
+    joinedBoardHandler = board => {
+        document.getElementById('server').innerHTML = board;
+        store.getStatus(board, status => {
+            console.log(status);
+            let uneditable = new Array(81);
+            status.uneditable.forEach(({ row, col }) => {
+                uneditable[row * 9 + col] = true;
+            });
+            this.setState({
+                uneditable,
+                time: status.time,
+                penalty: status.penalty,
+                finishTime: status.finishTime,
+            });
+        });
+    };
+
+    updatedCellsHandler = cells => {
+        const { puzzle } = this.state;
+        cells.forEach(({ row, col, val }) => (puzzle[row * 9 + col] = val));
+        this.setState({ puzzle });
+    };
+
+    submissionResultHandler = res => {
+        const { time, penalty, verdict, finishTime } = res;
+        this.setState({
+            showPopup: true,
+            time,
+            penalty,
+            verdict,
+            finishTime,
+        });
+    };
 
     componentDidMount() {
-        store.connectToServer((board) => {
-            document.getElementById("server").innerHTML = board;
-            store.getStatus(board, (status) => {
-                let uneditable = new Array(81);
-                status.uneditable.forEach(({ row, col }) => {
-                    uneditable[row * 9 + col] = true;
-                });
-                this.setState({
-                    uneditable,
-                    time: status.time,
-                    penalty: status.penalty,
-                    finishTime: status.finishTime,
-                });
-            });
-        });
-        store.createBoard();
-        store.subscribeToUpdatedCells((cells) => {
-            const { puzzle } = this.state;
-            cells.forEach(({ row, col, val }) => (puzzle[row * 9 + col] = val));
-            this.setState({ puzzle });
-        });
-        store.subscribeToSubmissionResult((res) => {
-            const { time, penalty, verdict, finishTime } = res;
-            this.setState({
-                showPopup: true,
-                time,
-                penalty,
-                verdict,
-                finishTime,
-            });
-        });
+        store.subscribeToJoinedBoard(this.joinedBoardHandler);
+        store.subscribeToUpdatedCells(this.updatedCellsHandler);
+        store.subscribeToSubmissionResult(this.submissionResultHandler);
         store.subscribeToErrorOccurred(alert);
+        store.connectToServer();
+        store.createBoard();
     }
 
     render() {
@@ -92,7 +99,7 @@ class App extends React.Component {
                         id="join-button"
                         onClick={() => {
                             const boardName = document.getElementById(
-                                "join-board"
+                                'join-board'
                             ).value;
                             store.joinBoard(boardName);
                         }}
@@ -114,15 +121,12 @@ class App extends React.Component {
                 </form>
                 <div id="check">
                     <div id="timer">
-                        Timer:{" "}
-                        {!this.state.time
-                            ? "0"
-                            : this.secondsToHms(this.state.time)}
+                        Timer: {!time ? '0' : secondsToHms(time)}
                     </div>
-                    <div id="penalty">Penalty: {this.state.penalty} </div>
+                    <div id="penalty">Penalty: {penalty} </div>
                     {finishTime >= 0 ? (
                         <div id="finish-time">
-                            Finish: {this.secondsToHms(finishTime)}{" "}
+                            Finish: {secondsToHms(finishTime)}{' '}
                         </div>
                     ) : null}
                     <button id="submit-board" onClick={store.submitBoard}>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import io from "socket.io-client";
-const BASE_URL = "";
+import axios from 'axios';
+import io from 'socket.io-client';
+const BASE_URL = 'http://localhost:3000';
 // TODO: comment above line and uncomment below line for dev and test on local machine
 // const BASE_URL = "https://wedoku.herokuapp.com";
 
@@ -11,59 +11,52 @@ let updatedCells = undefined;
 let submissionResult = undefined;
 let errorOccurred = undefined;
 
-export function connectToServer(jB, uC, sR, eO) {
-    joinedBoard = jB;
-    updatedCells = uC;
-    submissionResult = sR;
-    errorOccurred = eO;
-
+export function connectToServer() {
     socket = io.connect(BASE_URL);
 
-    socket.on("connect", function (data) {
-        console.log("Connected to server!");
+    socket.on('connect', function (data) {
+        console.log('Connected to server!');
     });
 
-    socket.on("joinedBoard", (channelName) => {
-        if (typeof joinedBoard === "function") {
+    socket.on('joinedBoard', channelName => {
+        if (typeof joinedBoard === 'function') {
             joinedBoard(channelName);
         }
     });
 
-    socket.on("updatedCells", (cells) => {
-        if (typeof updatedCells === "function") {
+    socket.on('updatedCells', cells => {
+        if (typeof updatedCells === 'function') {
             updatedCells(cells);
         }
     });
 
-    socket.on("submissionResult", (res) => {
-        console.log("Called hoise");
-        console.log(typeof submissionResult);
-        if (typeof submissionResult === "function") {
+    socket.on('submissionResult', res => {
+        if (typeof submissionResult === 'function') {
             submissionResult(res);
         }
     });
 
-    socket.on("errorOccurred", (err) => {
-        if (typeof errorOccurred === "function") {
+    socket.on('errorOccurred', err => {
+        if (typeof errorOccurred === 'function') {
             errorOccurred(err);
         }
     });
 }
 
 export function createBoard() {
-    socket.emit("createBoard");
+    socket.emit('createBoard');
 }
 
 export function joinBoard(channel) {
-    socket.emit("joinBoard", channel);
+    socket.emit('joinBoard', channel);
 }
 
 export function updateCell(row, col, val) {
-    socket.emit("updateCell", { row, col, val });
+    socket.emit('updateCell', { row, col, val });
 }
 
 export function submitBoard() {
-    socket.emit("submitBoard");
+    socket.emit('submitBoard');
 }
 
 export function subscribeToJoinedBoard(fn) {
@@ -85,6 +78,6 @@ export function subscribeToErrorOccurred(fn) {
 export function getStatus(boardName, fn) {
     axios
         .get(`${BASE_URL}/status/${boardName}`)
-        .then((res) => fn(res.data))
-        .catch((err) => alert(err));
+        .then(res => fn(res.data))
+        .catch(err => alert(err));
 }
